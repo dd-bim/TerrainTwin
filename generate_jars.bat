81362ddfdb61664db565972b507acab68e8054d0
Schilling, Sebastian <sebastian.schilling@htw-dresden.de>
2021-04-16 13:43:25 +0200

    changed eureka registration and credential management

--- generate_jars.bat
+++ generate_jars.bat
@@ -2,29 +2,29 @@ cd ConfigServer
 del "target\ConfigServer-1.0.jar"
 call mvn package -Dmaven.test.skip=true
 copy /Y "target\ConfigServer-1.0.jar" "..\files"
-@REM cd ..\EurekaServer
-@REM del "target\EurekaServer-1.0.jar" 
-@REM call mvn package -Dmaven.test.skip=true
-@REM copy /Y "target\EurekaServer-1.0.jar" "..\files"
+cd ..\EurekaServer
+del "target\EurekaServer-1.0.jar" 
+call mvn package -Dmaven.test.skip=true
+copy /Y "target\EurekaServer-1.0.jar" "..\files"
 cd ..\Csv2RdfConverterService 
 del "target\Csv2RdfConverterService-1.0.jar"
 call mvn package -Dmaven.test.skip=true
 copy /Y "target\Csv2RdfConverterService-1.0.jar" "..\files"
-@REM cd ..\MinIOUploadService 
-@REM del "target\MinIOUploadService-1.0.jar"
-@REM call mvn package -Dmaven.test.skip=true
-@REM copy /Y "target\MinIOUploadService-1.0.jar" "..\files"
-@REM cd ..\PostgresImportService 
-@REM del "target\PostgresImportService-1.0.jar"
-@REM call mvn package -Dmaven.test.skip=true
-@REM copy /Y "target\PostgresImportService-1.0.jar" "..\files"
-@REM cd ..\GraphDBImportService
-@REM del "target\GraphDBImportService-1.0.jar"
-@REM call mvn package -Dmaven.test.skip=true
-@REM copy /Y "target\GraphDBImportService-1.0.jar" "..\files"
-@REM cd ..\GatewayService
-@REM del "target\GatewayService-1.0.jar"
-@REM call mvn package -Dmaven.test.skip=true
-@REM copy /Y "target\GatewayService-1.0.jar" "..\files"
+cd ..\MinIOUploadService 
+del "target\MinIOUploadService-1.0.jar"
+call mvn package -Dmaven.test.skip=true
+copy /Y "target\MinIOUploadService-1.0.jar" "..\files"
+cd ..\PostgresImportService 
+del "target\PostgresImportService-1.0.jar"
+call mvn package -Dmaven.test.skip=true
+copy /Y "target\PostgresImportService-1.0.jar" "..\files"
+cd ..\GraphDBImportService
+del "target\GraphDBImportService-1.0.jar"
+call mvn package -Dmaven.test.skip=true
+copy /Y "target\GraphDBImportService-1.0.jar" "..\files"
+cd ..\GatewayService
+del "target\GatewayService-1.0.jar"
+call mvn package -Dmaven.test.skip=true
+copy /Y "target\GatewayService-1.0.jar" "..\files"
 cd ..
 cmd /k
--- push_dockerfiles.bat
+++ push_dockerfiles.bat
@@ -1,14 +1,14 @@
 docker tag configserver schi11er/tt_configserver
 docker push schi11er/tt_configserver
-@REM docker tag eurekaserver schi11er/tt_eurekaserver
-@REM docker push schi11er/tt_eurekaserver
-@REM docker tag gateway-service schi11er/tt_gateway-service
-@REM docker push schi11er/tt_gateway-service
+docker tag eurekaserver schi11er/tt_eurekaserver
+docker push schi11er/tt_eurekaserver
+docker tag gateway-service schi11er/tt_gateway-service
+docker push schi11er/tt_gateway-service
 docker tag csv2rdf-converter-service schi11er/tt_csv2rdf-converter-service
 docker push schi11er/tt_csv2rdf-converter-service
-@REM docker tag postgres-import-service schi11er/tt_postgres-import-service
-@REM docker push schi11er/tt_postgres-import-service
-@REM docker tag graphdb-import-service schi11er/tt_graphdb-import-service
-@REM docker push schi11er/tt_graphdb-import-service
-@REM docker tag minio-upload-service schi11er/tt_minio-upload-service
-@REM docker push schi11er/tt_minio-upload-service
+docker tag postgres-import-service schi11er/tt_postgres-import-service
+docker push schi11er/tt_postgres-import-service
+docker tag graphdb-import-service schi11er/tt_graphdb-import-service
+docker push schi11er/tt_graphdb-import-service
+docker tag minio-upload-service schi11er/tt_minio-upload-service
+docker push schi11er/tt_minio-upload-service
