00f6af7d16ace04952656a001a5b63f029bd2d5a
Schilling, Sebastian <sebastian.schilling@htw-dresden.de>
2021-07-16 14:34:28 +0200

    add test service

---
+++ TestService/src/main/java/com/Microservices/TestService/controller/TestRestController.java
@@ -0,0 +1,29 @@
+package com.Microservices.TestService.controller;
+
+import com.Microservices.TestService.service.Service;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RefreshScope
+@RestController
+@Tag(name = "Test API", description = "Documentation")
+public class TestRestController {
+
+  @GetMapping("/test/")
+  public String hello() {
+      return "Hello World!";
+  }
+
+  @PostMapping("/test/{name}")
+  public String helloService(@PathVariable String name) {
+    Service service = new Service();
+      return service.sayHello(name);
+  }
+
+
+}
